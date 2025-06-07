4f8e606463824181ad2b8f2c651c1c720966b548
nikhil6392 <nikhilpathak210431@gmail.com>
2025-06-07 09:23:28 +0530

    giving more robustness and error handling to the controllers

--- movie/controller.js
+++ movie/controller.js
@@ -3,13 +3,26 @@ import { getAll, remove } from "./model.js";
 import { render } from "./view.js";
 
 export async function listAction(req, res){
+  try {
     const data = await getAll();
     const body = render(data)
     res.send(body)
+    
+  } catch (error) {
+    res.status(500).send("Internal Server Error")
+  }
 }
 
 export async function removeAction(req, res) {
-    const id = parseInt(req.params.id, 10);
-    await remove(id);
-    res.redirect(req.baseUrl)
+    try {
+        const id = parseInt(req.params.id, 10);
+
+        if(isNaN(id)){
+            return res.status(400).send("Invalid Id")
+        }
+        await remove(id);
+        res.redirect(req.baseUrl)
+    } catch (error) {
+        res.status(500).send("Internal Server Error")
+    }
 }
